62435354a6ed45fe0bc3af120ccc6b7103a50fe5
David Delagneau <daviddelagneau@yahoo.es>
2019-11-09 16:03:52 -0600

    test: routes factory added

--- database/factories/RoutesFactory.php
+++ database/factories/RoutesFactory.php
@@ -2,11 +2,23 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Routes;
+use App\Route;
 use Faker\Generator as Faker;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
+use Grimzy\LaravelMysqlSpatial\Types\LineString;
 
-$factory->define(Routes::class, function (Faker $faker) {
+$factory->define(Route::class, function (Faker $faker) {
     return [
-        //
+        'start' => new Point($faker->latitude(12.14,12.92),$faker->longitude(-86,-84)),
+        'end' => new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+        'route' => new LineString([
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+            new Point($faker->latitude(12.14,13.92),$faker->longitude(-86,-84)),
+        ]),
     ];
 });
