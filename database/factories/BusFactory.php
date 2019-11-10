d8239bc3053432d39e369766aa0f81929b28d59e
David Delagneau <daviddelagneau@yahoo.es>
2019-11-09 19:19:48 -0600

    test: buses factory added

--- database/factories/BusFactory.php
+++ database/factories/BusFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Bus::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->firstName(),
+        'license_plate'=>strtoupper($faker->randomLetter().$faker->randomLetter()).'-'.$faker->randomNumber(5),
+        'device_id'=>$faker->randomNumber(5, true),
     ];
 });
