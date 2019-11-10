4bfb6a5589f3107cf02e9a8d063a82a351231171
David Delagneau <daviddelagneau@yahoo.es>
2019-11-09 22:33:45 -0600

    feat: Real time positions for buses

--- app/Bus.php
+++ app/Bus.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Client;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
 
 class Bus extends Model
 {
@@ -18,5 +20,27 @@ class Bus extends Model
     public function routes()
     {
         return $this->belongsToMany('App\Route')->using('App\BusRoute');
+    }
+    
+    /**
+     * Obtiene la posición actualizada
+     *
+     * @return string
+     */
+    public function getPositionAttribute()
+    {
+        $client = new Client([
+            'base_uri' => env('TRACCAR_HOST'),
+            'auth' => [env('TRACCAR_EMAIL'), env('TRACCAR_PASSWORD')]
+        ]);
+        $response = $client->request('GET','api/positions');
+        $devices = Collect(json_decode($response->getBody()->getContents()));
+        $device = $devices->firstWhere('id',$this->device_id);
+        if(isset($device)){
+            $position = new Point($device->latitude,$device->longitude);
+            return $position;
+        }else{
+            return response()->json('Dispositivo GPS no encontrado', 204);
+        }
     }
 }
