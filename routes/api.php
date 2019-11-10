49054efc13241f1a392ba8f675d7fddd54530823
David Delagneau <daviddelagneau@yahoo.es>
2019-11-10 05:30:35 -0600

    feat: Controller logic added

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResources([
     'routes'=>'RouteController',
     'buses'=>'BusController',
+    'stops'=>'StopController',
 ]);
