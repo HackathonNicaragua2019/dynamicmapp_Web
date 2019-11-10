c6e5f35516eb807e7f5718f3354258f0b17e57d2
David Delagneau <daviddelagneau@yahoo.es>
2019-11-09 22:05:58 -0600

    feat: Model bus and relationship route

--- app/Bus.php
+++ app/Bus.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bus extends Model
 {
-    //
+    protected $fillable=[
+        'name',
+        'license_plate',
+        'device_id',
+    ];
+
+    /**
+     * Las rutas que pertenecen al autobús.
+     */
+    public function routes()
+    {
+        return $this->belongsToMany('App\Route')->using('App\BusRoute');
+    }
 }
