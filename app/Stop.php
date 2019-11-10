e348173bf0546bd54d0b6aa7ae60dda6bcd40fb4
David Delagneau <daviddelagneau@yahoo.es>
2019-11-10 09:02:11 -0600

    feat: Relationship rutas to many stops

--- app/Stop.php
+++ app/Stop.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Grimzy\LaravelMysqlSpatial\Eloquent\SpatialTrait;
+use Grimzy\LaravelMysqlSpatial\Types\Point;
 
 class Stop extends Model
 {
@@ -11,9 +12,18 @@ class Stop extends Model
     
     protected $fillable=[
         'position',
+        'route_id',
     ];
 
     protected $spatialFields=[
         'position',
     ];
+
+    /**
+     * Obtiene la ruta que pertence a la parada.
+     */
+    public function route()
+    {
+        return $this->belongsTo('App\Route');
+    }
 }
